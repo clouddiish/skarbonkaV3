3a72b3ab20068f4c12b8fb2f0506aabfb4a7c7f0
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-11-02 10:26:33 +0100

    fixed position of summary cards

--- frontend/src/components/TransactionSummary.js
+++ frontend/src/components/TransactionSummary.js
@@ -1,11 +1,12 @@
 import TransactionSummaryCard from "./TransactionSummaryCard";
+import "./TransactionSummary.css";
 
 export default function TransactionSummary({ calculateTransactionsSum }) {
   let income = calculateTransactionsSum("income");
   let spending = calculateTransactionsSum("expense");
 
   return (
-    <div className="row">
+    <div className="row summary-card">
       <TransactionSummaryCard title="income" value={income} />
       <TransactionSummaryCard title="expenses" value={spending} />
       <TransactionSummaryCard title="balance" value={income - spending} />
